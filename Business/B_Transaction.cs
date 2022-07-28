e5a65d46fd04dc1f89f3de60a81133bc54fd1a43
Jorge Ariel Garrone <jorge.garrone@gmail.com>
2022-07-28 17:08:44 -0400

    Add: Create Business layer

---
+++ Business/B_Transaction.cs
@@ -0,0 +1,38 @@
+﻿using DataAccess;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business
+{
+    public class B_Transaction
+    {
+        public static List<TransactionEntity> OutputList()
+        {
+            using (var db = new InventoryContext())
+            {
+                return db.InOuts.ToList();
+            }
+        }
+
+        public static void CreateOutput(TransactionEntity oOutput)
+        {
+            using (var db = new InventoryContext())
+            {
+                db.InOuts.Add(oOutput);
+                db.SaveChanges();
+            }
+        }
+
+        public static void UpdateOutput(TransactionEntity oOutput)
+        {
+            using (var db = new InventoryContext())
+            {
+                db.InOuts.Update(oOutput);
+                db.SaveChanges();
+            }
+        }
+    }
+}
